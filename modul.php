bc5f81d30e6abcc4d0278a7982940536ffd77a2b
We <hegedus.karoly@wetalkdigital.hu>
2024-05-16 00:25:23 +0200

    Add files via upload

---
+++ modul.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Modul Bővítmény
+Plugin URI: https://wetalkdigital.hu/
+Description: Egyedi modulok a WordPress weboldalhoz.
+Version: 1.0
+Author: WeTalkDigital
+Author URI: mailto:info@wetalkdigital.hu
+Requires at least: 6.0
+Tested up to: 6.2
+Requires PHP: 8.0
+*/
+
+
+require plugin_dir_path(__FILE__) . 'blog.php';
+require plugin_dir_path(__FILE__) . 'fixgombok.php';
+require plugin_dir_path(__FILE__) . 'accordion.php';
+
+
